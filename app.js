00680730a069bb1108d30bff8c21ddf283a0b9aa
WeiduoS <ws1255@nyu.edu>
2019-09-27 17:58:11 -0400

    add a navigation bar

---
+++ app.js
@@ -0,0 +1,8 @@
+var url = " /Users/Stoic_Colony/Documents/GitHub/JSWork/app.js";
+
+function log(str) {
+  console.log(str);
+}
+
+module.exports.log = log;
+module.exports.url = url;
---
+++ node.js
@@ -0,0 +1,8 @@
+var hello = function() {
+  console.log("hellp");
+};
+var message = "";
+// hello();
+// global.hello();
+console.log(global.message);
+console.log(global.hello());
