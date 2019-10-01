d773c5ea58ac473690b9c7fe5a37c4f186bbfe8a
WeiduoS <ws1255@nyu.edu>
2019-10-01 16:26:22 -0400

    update

--- reuseable_components/navigation_bar/nav_01/navigation_bar.js
+++ reuseable_components/navigation_bar/nav_01/navigation_bar.js
@@ -2,11 +2,6 @@ window.onload = function () {
     let toggle_button = document.getElementById("nav-toggle-button");
     let collapse_list = document.getElementsByClassName("nav-collapse")[0];
     toggle_button.onclick = function () {
-        // if(collapse_list.style.display !== "block") {
-        //     collapse_list.style.display = "block";
-        // }else{
-        //     collapse_list.style.display = "none";
-        // }
         if(toggle_button.classList.contains("active")) {
             toggle_button.classList.remove("active");
         }else{
@@ -17,5 +12,23 @@ window.onload = function () {
         }else {
             collapse_list.classList.add("show");
         }
+    }
+};
+window.onscroll = function() {
+    // make top nav change background color when scroll
+    let navbar = document.getElementsByClassName("navigation")[0];
+    let sticky = navbar.offsetTop + navbar.clientHeight;
+
+    let navCollapse = document.getElementsByClassName("nav-collapse")[0];
+    let navband_image = document.getElementsByClassName("nav-brand-img")[0];
+
+    if (window.pageYOffset > sticky + 120) {
+        navband_image.classList.add("shrink");
+        navCollapse.children[0].classList.add("rising-up");
+        navCollapse.children[1].classList.add("rising-up");
+    } else {
+        navband_image.classList.remove("shrink");
+        navCollapse.children[0].classList.remove("rising-up");
+        navCollapse.children[1].classList.remove("rising-up");
     }
 };
