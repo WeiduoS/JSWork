c05f767e1043e9ccc61c1d7001b06f81ab402de5
WeiduoS <ws1255@nyu.edu>
2019-10-14 15:52:17 -0400

    udpate

--- reuseable_components/navigation_bar/nav_02/navigation_bar.js
+++ reuseable_components/navigation_bar/nav_02/navigation_bar.js
@@ -1,24 +1,16 @@
-// var open = document.getElementById('hamburger');
-// var changeIcon = true;
-//
-// open.addEventListener("click", function(){
-//
-//     var overlay = document.querySelector('.overlay');
-//     var nav = document.querySelector('nav');
-//     var icon = document.querySelector('.menu-toggle i');
-//
-//     overlay.classList.toggle("menu-open");
-//     nav.classList.toggle("menu-open");
-//
-//     if (changeIcon) {
-//         icon.classList.remove("fa-bars");
-//         icon.classList.add("fa-times");
-//
-//         changeIcon = false;
-//     }
-//     else {
-//         icon.classList.remove("fa-times");
-//         icon.classList.add("fa-bars");
-//         changeIcon = true;
-//     }
-// });
+window.onload = function() {
+  let toggle_button = document.getElementById("nav-toggle-button");
+  let collapse_list = document.getElementsByClassName("nav-list")[0];
+  toggle_button.onclick = function() {
+    if (toggle_button.classList.contains("active")) {
+      toggle_button.classList.remove("active");
+    } else {
+      toggle_button.classList.add("active");
+    }
+    if (collapse_list.classList.contains("collapse")) {
+      collapse_list.classList.remove("collapse");
+    } else {
+      collapse_list.classList.add("collapse");
+    }
+  };
+};
